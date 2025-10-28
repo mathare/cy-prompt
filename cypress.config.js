46ce317c987e6e6964c01bda126ddbb6b5bfaac4
Mat Hare <47448254+mathare@users.noreply.github.com>
2025-10-28 14:36:54 +0000

    Enable cy.prompt

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    experimentalPromptCommand: true,
+    projectId: 'rchav9',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
